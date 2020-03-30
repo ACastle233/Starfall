2cbf97c731159ceeda123a69a97ac42cc81708cf
ACastle233 <ACastle233@users.noreply.github.com>
2020-03-29 21:03:27 -0600

    Enemies
    Se añadio enemigos y sistema de particulas para explision y arma

--- Starfall/Assets/Player.cs
+++ Starfall/Assets/Player.cs
@@ -31,29 +31,32 @@ public class Player : MonoBehaviour
 
     private void ProcessRotation()
     {
-        float pitchDueToPosition = transform.localRotation.y * positionPitchFactor;
-        float pitchDueControlThrow =  yThrow * controlPitchFactor;
-        float pitch = pitchDueToPosition + pitchDueControlThrow;
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
 
         float yaw = transform.localPosition.x * positionYawFactor;
 
         float roll = xThrow * controlRollFactor;
 
-        transform.localRotation = Quaternion.Euler(pitch , yaw, roll);
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     private void ProcessTranslation()
     {
         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
         float xOffset = xThrow * Speed * Time.deltaTime;
         float yOffset = yThrow * Speed * Time.deltaTime;
 
         float rawXPos = transform.localPosition.x + xOffset;
-        float rawYPos = transform.localPosition.y + yOffset;
-
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float rawYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
+
