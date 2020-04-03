d0814fdefa931add0c41e96ae60c463a6c4f0f53
ACastle233 <ACastle233@users.noreply.github.com>
2020-04-03 16:44:48 -0600

    Process Hit

--- Starfall/Assets/Scripts/Enemy.cs
+++ Starfall/Assets/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 12;
+    [SerializeField] int hits = 3;
 
     bool isDead = false;
     ScoreBoard scoreBoard;
@@ -26,15 +27,24 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
-        //Add something
-        while (!isDead)
+        ProcessHit();
+        if (hits <= 0)
         {
-            scoreBoard.ScoreHit(scorePerHit);
-            GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
-            fx.transform.parent = parent;
-            isDead = true;
-            Destroy(gameObject);
+            KillEnemy();
         }
-        
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.ScoreHit(scorePerHit);
+        hits = hits - 1;
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        isDead = true;
+        Destroy(gameObject);
     }
 }
